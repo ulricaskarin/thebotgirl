38b95b36065a761c8bab31168b25bcd7ea0c711e
ulricaskarin <ls222xp@student.lnu.se>
2016-03-31 22:46:19 +0200

    Having another try with heroku deploy

--- app.js
+++ app.js
@@ -4,16 +4,12 @@
 var Twit = require('twit');
 
 // Search phrase:
-var searchPhrase = process.env.TWITTER_SEARCH_PHRASE;
+var searchPhrase = "#react min_retweets:5 OR javascript min_retweets:5 OR #nodejs min_retweets:8 OR #jQuery min_retweets:5 OR #angular min_retweets:5 OR #mongoose min_retweets:5 filter:links"
 
 function Bot() {
 
     // Api tokens for twitter:
-    this.twitter = new Twit({
-        consumer_key: process.env.CONSUMER_KEY,
-        consumer_secret: process.env.CONSUMER_SECRET,
-        access_token: process.env.ACCESS_TOKEN,
-        access_token_secret: process.env.ACCESS_TOKEN_SECRET});
+    this.twitter = new Twit();
 
     this.searchPhrase = searchPhrase;
 }
